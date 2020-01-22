1932ddec1d80092eb37206bb1f4145e72a7d5323
ghd6845 <ghd6845@gmail.com>
2020-01-22 16:12:01 +0900

    node test

---
+++ syntax/sync.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+/* 
+//동기적
+//readfileSync
+console.log('A');
+var result = fs.readFileSync('syntax/sample.txt','utf8');
+console.log(result);
+console.log('C');
+*/
+
+//비동기적
+console.log('A');
+fs.readFile('syntax/sample.txt','utf8',function(err,result){
+    console.log(result);
+});
+console.log('C');
